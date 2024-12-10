5f43dc0c6ba861c1dd11481783066510dacb6ed2
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-10 18:08:51 +0000

    addBook, removeBook, searchBook, listAllBooks done

--- app/src/main/java/org/example/Library.java
+++ app/src/main/java/org/example/Library.java
@@ -13,6 +13,7 @@ public class Library {
             if (booksInStore.get(i).getTitle().equals(book.getTitle())) {
                 booksInStore.remove(i);
                 booksInStore.add(book);
+                System.out.println("This book has been added to the library");
                 return;
             }
         }
@@ -20,5 +21,44 @@ public class Library {
         booksInStore.add(book);
     }
 
+    //Method to remove a book object
+    public void removeBook(String title) {
+        boolean found = false;
+
+        for (int i = 0; i < booksInStore.size(); i++) {
+            if (booksInStore.get(i).getTitle().equals(title)) {
+                booksInStore.remove(i);
+                System.out.println("This book has been removed from the library");
+                found = true;
+                return;
+            }
+        }
+
+        if (!found) System.out.println("This book does not exist in the library");
+    }
+
+    //Method to search for a book
+    public void searchBook(String title) {
+        boolean found = false;
+
+        for (Book book : booksInStore) {
+            if (book.getTitle().equals(title)) {
+                // print out the details of the found book
+                System.out.println("Title:" + book.getTitle());
+                System.out.println("Author:" + book.getAuthor());
+                System.out.println("Year Published:" + book.getYearPublished());
+
+                found = true;
+                return;
+            }
+        }
+
+        if (!found) System.out.println("This book does not exist in the library");
+    }
+
+    //Method to list all books
+    public void listAllBooks() {
+        for (Book book : booksInStore) System.out.println(book);
+    }
 
 }

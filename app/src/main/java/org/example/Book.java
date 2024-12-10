b2fce52b9245d7f0d6904fc45efd173517137bd5
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2024-12-10 16:28:29 +0000

    started working on the book class

--- app/src/main/java/org/example/Book.java
+++ app/src/main/java/org/example/Book.java
@@ -1,5 +1,43 @@
 public class Book {
-
-    // This is your working class.
     
+    //Initializing Variables
+    private String title;
+    private String author;
+    private int yearPublished;
+    private boolean isBorrowed = false;
+
+    //Constructor Function
+    public Book(String title, String author, int yearPublished) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+    }
+
+    //Title Getter and Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    //Author Getter and Setter
+    public String Author() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    //YearPublished Getter and Setter
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
 }
